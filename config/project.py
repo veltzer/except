d8dffeebc99edd0a9ae99f894cd5f7cd43874ec3
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:59 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "except"
-project_long_description = "exception library for C"
-project_year_started = 2011
-project_description = """exception library for C"""
+description_short = "exception library for C"
